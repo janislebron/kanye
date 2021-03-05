36e70f7ec36580e177e38ddc6386429bd3e9a320
janislebron <64046343+janislebron@users.noreply.github.com>
2021-03-05 05:17:54 -0600

    Update CLI.rb
    asking user what they would like to do next

--- lib/kanye/CLI.rb
+++ lib/kanye/CLI.rb
@@ -8,13 +8,14 @@ class CLI
     puts "!!!! Are you ready to be great today?"
     daily_quote
     input
+    more
   end
 
   def input
     #get user input
     puts "Type 'enlightened' to receive your daily nugget of gold."
     user_input = gets.strip.downcase
-    if user_input == enlightened
+    if user_input == "enlightened"
       then Kanye
     else
       puts "How else would you to level up today..?"
@@ -23,7 +24,13 @@ class CLI
 
   end
 
-  def next
+  def more
     #what user would like to do next
+    puts "Would you like to receive more knowledge today?"
+    puts "Type 'y' if you love yourself."
+    if user_input == "y"
+      then Kanye
+    else "Maybe you'll feel more inspired tomorrow."
+    end
   end
 end
