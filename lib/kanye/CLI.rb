f1a663bd26aa05015c2f3cd9a59aa7b52246fd81
janislebron <64046343+janislebron@users.noreply.github.com>
2021-03-05 05:09:46 -0600

    Update CLI.rb
    skeleton of user control flow

--- lib/kanye/CLI.rb
+++ lib/kanye/CLI.rb
@@ -4,7 +4,26 @@ class CLI
   #controlling flow of program
 
 
-  def welcome
+  def start
     puts "!!!! Are you ready to be great today?"
+    daily_quote
+    input
+  end
+
+  def input
+    #get user input
+    puts "Type 'enlightened' to receive your daily nugget of gold."
+    user_input = gets.strip.downcase
+    if user_input == enlightened
+      then Kanye
+    else
+      puts "How else would you to level up today..?"
+    end
+  end
+
+  end
+
+  def next
+    #what user would like to do next
   end
 end
