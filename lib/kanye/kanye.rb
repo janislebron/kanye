8f4b3625de3321cf80f388895b0f125f9e37b37d
janislebron <64046343+janislebron@users.noreply.github.com>
2021-03-05 05:00:21 -0600

    Update kanye.rb
    initialized quotes for Kanye class

--- lib/kanye/kanye.rb
+++ lib/kanye/kanye.rb
@@ -1,3 +1,15 @@
 class Kanye
   #receives Kanye quote
+  attr_accessor :quote
+  @@all = []
+
+  def initialize
+    @quote = quote
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
