951bd351f4a195b5a1ee788e7962b54d5437cfe3
janislebron <64046343+janislebron@users.noreply.github.com>
2021-03-08 05:34:04 -0600

    added start file to jumpstart gem

--- lib/environment.rb
+++ lib/environment.rb
@@ -3,13 +3,14 @@ require 'net/http'
 require 'json'
 
 
-## require all files that we will use
+## require all files that will be used
 
 require_relative "./lib/API"
 require_relative "./lib/CLI"
 require_relative "./lib/kanye"
 
 require "kanye/version"
+
 
 module Kanye
   class Error < StandardError; end
